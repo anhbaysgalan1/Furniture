b3faeac7cd976fb07c0808052e806f1104615943
working <nguyenmanhtoan1501@gmail.com>
2019-10-19 11:01:39 -0700

    ddd

--- frontend/src/reducers/RootReducer.jsx
+++ frontend/src/reducers/RootReducer.jsx
@@ -4,22 +4,7 @@ import LoadingReducer from './LoadingReducer';
 import UtilityReducer from './UtilityReducer';
 import UserReducer from './UserReducer';
 import GroupReducer from './GroupReducer';
-import AreaReducer from './AreaReducer';
-import RoleReducer from './RoleReducer';
-import PermissionReducer from './PermissionReducer';
-import PositionReducer from './PositionReducer';
-import SettingReducer from './SettingReducer';
-import ConstructionReducer from './ConstructionReducer';
-import LaborReducer from './LaborReducer';
-import HistoryReducer from './HistoryReducer';
-import WorkerReducer from './WorkerReducer';
-import RequestReducer from './RequestReducer';
-import HourReducer from './HourReducer';
-import CalendarReducer from './CalendarReducer';
-import TimekeepingReducer from './TimekeepingReducer';
-import ContentsReducer from './ContentsReducer';
-/*__AUTOIMPORTREDUCER__*/
-/*Do not remove  __AUTOIMPORTREDUCER__ variable.*/
+
 
 export default combineReducers({
   i18n: i18nReducer,
@@ -27,20 +12,4 @@ export default combineReducers({
   utility: UtilityReducer,
   user: UserReducer,
   group: GroupReducer,
-  area: AreaReducer,
-  role: RoleReducer,
-  permission: PermissionReducer,
-  position: PositionReducer,
-  setting: SettingReducer,
-  construction: ConstructionReducer,
-  labor: LaborReducer,
-  history: HistoryReducer,
-  worker: WorkerReducer,
-  request: RequestReducer,
-  hour: HourReducer,
-  calendar: CalendarReducer,
-  timekeeping: TimekeepingReducer,
-  contents: ContentsReducer,
-  /*__AUTOREDUCER__*/
-  /*Do not remove  __AUTOREDUCER__ variable.*/
 });
